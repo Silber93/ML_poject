42b567676c3a92b5c32c585c9f57c7a27f937446
ooori14 <ooori14@campus.technion.ac.il>
2020-09-22 10:53:32 +0300

    hi

--- BNpgm.py
+++ BNpgm.py
@@ -42,7 +42,7 @@ c = 0
 size = 0
 for index, r in df_test.iterrows():
     # casual reasoning
-    prob = bnlearn.inference.fit(DAG, variables=['label'], evidence={'top actor': r['top actor'],
+    prob = bnlearn.inference.fit(DAG, variables=['label'], evidence={'top director': r['top director'],
                                                                      'budget scale': r['budget scale']})
     score = prob.values[1]
     real_label = r['label']
