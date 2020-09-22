5f9168ef6344a4d219ab4f00fb5b2c743183638e
ooori14 <ooori14@campus.technion.ac.il>
2020-09-22 14:37:50 +0300

    hi

--- analysis.py
+++ analysis.py
@@ -72,7 +72,7 @@ c = 0
 size = 0
 for index, r in df_test.iterrows():
     # evidential reasoning
-    prob = bnlearn.inference.fit(DAG, variables=['label'], evidence={'main genre': r['main genre'],
+    prob = bnlearn.inference.fit(DAG, variables=['label'], evidence={'budget scale': r['budget scale'],
                                                                      'pop scale': r['pop scale']})
 
     score = prob.values[1]
